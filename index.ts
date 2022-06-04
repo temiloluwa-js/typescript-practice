fb7a8ec2628e263f3a3de53bfdee49ea99e8e197
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-04 10:36:32 +0100

    playing with interfaces

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
 const obj = {width: 10, height: 15};
 const area = obj.width * obj.height
+
+console.log(4 / [])
+
+interface User {
+    name: string,
+    id: number,
+}
+
+const user: User = {
+    name: "Hayes",
+    id: "a"
+}
