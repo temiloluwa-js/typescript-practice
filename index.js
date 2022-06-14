51f1de28e5dedd3623261662a7dfd7125546e228
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-14 07:51:02 +0100

    studying TypeScript types

---
+++ index.js
@@ -0,0 +1,4 @@
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date}`);
+}
+let myName = "Hello";
---
+++ index.ts
@@ -0,0 +1,5 @@
+function greet(person: string, date: Date){
+    console.log(`Hello ${person}, today is ${date}`)
+}
+
+let myName: string = "Hello"
