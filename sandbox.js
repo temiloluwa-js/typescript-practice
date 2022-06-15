abc0f3719ca5557ef36dd51455f0058ebff550d3
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-15 17:26:48 +0100

    dynamic types

--- sandbox.js
+++ sandbox.js
@@ -11,10 +11,28 @@ var circ = function (diameter) {
 console.log(circ(20));
 var names = ['luigi', 'mario', 3];
 names.push(3);
-var mixed = ['ken', 4, 'chun-li'];
+// let mixed = ['ken', 4, 'chun-li']
+var mixedmixed = [];
+var ninjago;
 var ninja = {
     name: 'Mario',
     age: 5
 };
 ninja.name = String(9);
 console.log(typeof ninja.name, ninja.name);
+//explicit ttypes
+var char;
+var isLoggedIn;
+var nombas = [];
+char = "1";
+nombas = [1, 2, 3];
+console.log(nombas);
+var mixed = [];
+var ninjaOne;
+ninjaOne = { name: "yoshi", age: 30 };
+var ninjaTwo;
+// mixed.push(s)
+var age = 25;
+age = true;
+console.log(age);
+age = { name: "luigi", age: 30 };
--- sandbox.ts
+++ sandbox.ts
@@ -15,7 +15,9 @@ console.log(circ(20))
 const names = ['luigi', 'mario', 3]
 names.push(3)
 
-let mixed = ['ken', 4, 'chun-li']
+// let mixed = ['ken', 4, 'chun-li']
+let mixedmixed: any[] = []
+let ninjago: {name: any, age: any}
 
 let ninja = {
     name: 'Mario',
@@ -24,3 +26,28 @@ let ninja = {
 
 ninja.name = String(9)
 console.log(typeof ninja.name, ninja.name)
+
+//explicit ttypes
+let char: string;
+let isLoggedIn: boolean
+let nombas: number[] = []
+char = "1"
+nombas = [1, 2 ,3]
+console.log(nombas)
+
+let mixed: (string|number)[] = []
+let ninjaOne: object
+
+ninjaOne = {name: "yoshi", age: 30}
+let ninjaTwo: {
+    name: string,
+    age: number,
+    beltColor: string
+}
+// mixed.push(s)
+
+let age: any = 25;
+
+age = true
+console.log(age)
+age = {name: "luigi", age: 30}
