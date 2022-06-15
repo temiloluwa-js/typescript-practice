d5e21f5bb0a5a78e06ca0656e27f7f3ea505a75e
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-15 21:56:43 +0100

    classes

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,46 @@
-const anchor = document.querySelector("a")!;
-console.log(anchor.href);
+// const anchor = document.querySelector("a")!;
+// console.log(anchor.href);
+// const form = 
+
+// const form = document.querySelector('form')!
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement
+// console.log(form.children)
+
+const type = <HTMLSelectElement>document.querySelector('#type')
+const tofrom = <HTMLInputElement>document.querySelector('#tofrom')
+const details = <HTMLInputElement>document.querySelector('#details')
+const amount = <HTMLInputElement>document.querySelector('#amount')
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number){
+        this.client = c
+        this.details = d
+        this.amount =a
+    }
+
+    format(){
+        return `${this.client} breh ${this.details} breh ${this.amount}`
+    }
+}
+
+const invOne = new Invoice('mario', 'works on mario website', 250)
+const invTwo = new Invoice('luigi', 'works on luigi website', 200)
+console.log(invOne, invTwo)
+
+let invoices: Invoice[] = [];
+invoices.push(invOne, invTwo)
