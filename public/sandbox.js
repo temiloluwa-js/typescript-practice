85829d044f6b1f797348b52be5898b8e632814f4
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-15 17:41:08 +0100

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,38 +1,39 @@
-var character = "luigi";
+"use strict";
+const character = "luigi";
 console.log(character);
-var inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('input');
 console.log(inputs);
-inputs.forEach(function (input) {
+inputs.forEach(input => {
     console.log(input.offsetLeft);
 });
-var circ = function (diameter) {
+const circ = (diameter) => {
     return diameter * Math.PI;
 };
 console.log(circ(20));
-var names = ['luigi', 'mario', 3];
+const names = ['luigi', 'mario', 3];
 names.push(3);
 // let mixed = ['ken', 4, 'chun-li']
-var mixedmixed = [];
-var ninjago;
-var ninja = {
+let mixedmixed = [];
+let ninjago;
+let ninja = {
     name: 'Mario',
     age: 5
 };
 ninja.name = String(9);
 console.log(typeof ninja.name, ninja.name);
 //explicit ttypes
-var char;
-var isLoggedIn;
-var nombas = [];
+let char;
+let isLoggedIn;
+let nombas = [];
 char = "1";
 nombas = [1, 2, 3];
 console.log(nombas);
-var mixed = [];
-var ninjaOne;
+let mixed = [];
+let ninjaOne;
 ninjaOne = { name: "yoshi", age: 30 };
-var ninjaTwo;
+let ninjaTwo;
 // mixed.push(s)
-var age = 25;
+let age = 25;
 age = true;
 console.log(age);
 age = { name: "luigi", age: 30 };
