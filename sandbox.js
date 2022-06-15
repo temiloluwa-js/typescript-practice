9fa54423a07ccc1a618bbe0b64977e202a98020e
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-15 14:50:06 +0100

    objects & arrays

--- sandbox.js
+++ sandbox.js
@@ -9,3 +9,12 @@ var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(20));
+var names = ['luigi', 'mario', 3];
+names.push(3);
+var mixed = ['ken', 4, 'chun-li'];
+var ninja = {
+    name: 'Mario',
+    age: 5
+};
+ninja.name = String(9);
+console.log(typeof ninja.name, ninja.name);
--- sandbox.ts
+++ sandbox.ts
@@ -11,3 +11,16 @@ const circ = (diameter: number) => {
     return diameter * Math.PI
 }
 console.log(circ(20))
+
+const names = ['luigi', 'mario', 3]
+names.push(3)
+
+let mixed = ['ken', 4, 'chun-li']
+
+let ninja = {
+    name: 'Mario',
+    age: 5
+}
+
+ninja.name = String(9)
+console.log(typeof ninja.name, ninja.name)
